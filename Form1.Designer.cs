307b5532e4dfe4c42bd9752b1602f795c07ee163
Ghaith Tish <ghaith-tish@hotmail.com>
2019-10-18 13:40:46 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace XMLSchreiber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EingabeFeld = new System.Windows.Forms.ListBox();
+            this.Übernehmen_button1 = new System.Windows.Forms.Button();
+            this.Speichern_button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EingabeFeld
+            // 
+            this.EingabeFeld.FormattingEnabled = true;
+            this.EingabeFeld.Location = new System.Drawing.Point(12, 9);
+            this.EingabeFeld.Name = "EingabeFeld";
+            this.EingabeFeld.Size = new System.Drawing.Size(149, 329);
+            this.EingabeFeld.TabIndex = 0;
+            // 
+            // Übernehmen_button1
+            // 
+            this.Übernehmen_button1.Location = new System.Drawing.Point(168, 55);
+            this.Übernehmen_button1.Name = "Übernehmen_button1";
+            this.Übernehmen_button1.Size = new System.Drawing.Size(148, 23);
+            this.Übernehmen_button1.TabIndex = 1;
+            this.Übernehmen_button1.Text = "Übernehmen";
+            this.Übernehmen_button1.UseVisualStyleBackColor = true;
+            this.Übernehmen_button1.Click += new System.EventHandler(this.Übernehmen_button1_Click);
+            // 
+            // Speichern_button2
+            // 
+            this.Speichern_button2.Location = new System.Drawing.Point(168, 84);
+            this.Speichern_button2.Name = "Speichern_button2";
+            this.Speichern_button2.Size = new System.Drawing.Size(148, 23);
+            this.Speichern_button2.TabIndex = 2;
+            this.Speichern_button2.Text = "Speichern";
+            this.Speichern_button2.UseVisualStyleBackColor = true;
+            this.Speichern_button2.Click += new System.EventHandler(this.Speichern_button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 345);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Speichern_button2);
+            this.Controls.Add(this.Übernehmen_button1);
+            this.Controls.Add(this.EingabeFeld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox EingabeFeld;
+        private System.Windows.Forms.Button Übernehmen_button1;
+        private System.Windows.Forms.Button Speichern_button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XMLSchreiber
+{
+    public partial class Form1 : Form
+    {
+        string xmlDateiname;
+        public Form1()
+        {
+            InitializeComponent();
+            xmlDateiname = System.IO.Path.ChangeExtension(Application.ExecutablePath, ".xml");
+        }
+
+        private void Übernehmen_button1_Click(object sender, EventArgs e)
+        {
+            EingabeFeld.Items.Add(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void Speichern_button2_Click(object sender, EventArgs e)
+        {
+            XMLSchreiben();
+        }
+
+        void XMLSchreiben()
+        {
+            XmlWriterSettings Einstellungen = new XmlWriterSettings();
+            Einstellungen.Indent = true ;
+
+            int NumEingaben = EingabeFeld.Items.Count;
+
+            //eine Instanz von XMLwriter erstellen
+            XmlWriter XMLschreiber = XmlWriter.Create(xmlDateiname, Einstellungen);
+
+            XMLschreiber.WriteStartDocument();
+            XMLschreiber.WriteStartElement("HauptEintrag");
+            for (int i=0; i< NumEingaben; i++)
+            {
+                XMLschreiber.WriteStartElement("eintrag");
+                XMLschreiber.WriteElementString("name", Convert.ToString(EingabeFeld.Items[i]));
+                XMLschreiber.WriteEndElement();
+            }
+            XMLschreiber.WriteEndElement();
+            XMLschreiber.WriteEndDocument();
+            XMLschreiber.Close();
+
+        }
+    }
+}
